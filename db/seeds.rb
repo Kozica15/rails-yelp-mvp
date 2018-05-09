3db5ce7325cc2d160e3d8ca54199a9143707a50a
claire fumey <fumey_claire@hotmail.com>
2018-05-09 20:49:41 +0200

    mvc

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+# This file should contain all the record creation needed to seed the database with its default values.
+# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
+#
+# Examples:
+#
+#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
+#   Character.create(name: 'Luke', movie: movies.first)
+# db/seeds.rb
+
+puts 'Cleaning database...'
+Restaurant.destroy_all
+
+puts 'Creating restaurants...'
+restaurants_attributes =[]
+5.times do
+
+  hash_restaurant = {
+    name:         Faker::Hobbit.character,
+    address:      Faker::Address.street_address ,
+   category:   ["chinese", "italian", "japanese", "french", "belgian"].sample
+
+
+  }
+  restaurants_attributes << hash_restaurant
+
+
+end
+Restaurant.create!(restaurants_attributes)
+puts 'Finished!'
